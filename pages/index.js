958afbae7c41034febea0e3fff85ca6dbef4c381
Alexandre Peres <132930302+AlexandreP23@users.noreply.github.com>
2025-06-11 23:30:56 +0000

    texto trocado

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return <h1>Obrigado por ser a melhor namorada, a melhor esposa, a melhor mulher
+        que eu poderia ter encontrado nessa vida!!!Te amo, meu amor 💕🌹
+    </h1>
+}
+
+export default Home;
+
