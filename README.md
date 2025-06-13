0f22549fc4da1cee95aee3aa5d3dd4df29af1db8
Alexandre Peres <132930302+AlexandreP23@users.noreply.github.com>
2025-06-13 16:46:19 +0000

    adiciona configuração prettier `lint:check` e `lit:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do TabNews https://www.tabnews.com.br para o curso .Dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 function Home() {
-    return <h1>Projeto TabNews</h1>
+  return <h1>Projeto TabNews</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
-
